5ae350c38cdb814d10c846d4df9bfcadee7c0703
blam4521 <blam452@gmail.com>
2015-10-23 00:06:15 -0700

    changed the format of the spacing

--- Convert_longHand_b.py
+++ Convert_longHand_b.py
@@ -7,71 +7,42 @@ import shutil
 
 def convert_jpg(img_dir, image_format, scale_img):
 
-		'''
-		Takes a img_dir, looks through every image file extension set by the users and changes it according to their
-		input scale
-		'''
+	'''
+	Takes a img_dir, looks through every image file extension set by the users and changes it according to their
+	input scale
+	'''
+	subdirectory = img_dir + "/texture_small"
+	os.makedirs(subdirectory)
+	
 
-		#img_files = [f for f in os.listdir(img_dir) if f.endswith('JPG')]
-		 
-		#img_files = []
+	for aFile in os.listdir(img_dir):
+		#print aFile
+		if aFile.endswith(image_format):
+			#print "this is a jpg"
+			img_file = Image.open(img_dir+"/"+aFile).convert('RGB')
+			#print "img file: ", img_file
+			width, height = (int(scale_img * img_file.size[0] / 100.0), 
+													int(scale_img * img_file.size[1] / 100.0))
 
-		subdirectory = img_dir + "/texture_small"
-		os.makedirs(subdirectory)
-		
+			print "resizing: ", aFile, " to width ", width, " x height ", height 
+			img_file = img_file.resize((width, height), Image.ANTIALIAS)
+			try:
+				fileName = os.path.splitext(aFile)[0] #without file extension
+				img_file.save(img_dir+"/"+ fileName +"_resize.png",format="png", 
+											optimize=True, 
+											quality=100, 
+											progressive=True)
+				for i in os.listdir(img_dir):
+					if i.endswith('_resize.png'):
+						print i
+						src_file = os.path.join(img_dir, i)
+						dst_file = os.path.join(subdirectory, i)
+						shutil.move(src_file, dst_file)
+				#print img_files
+			
+			except IOError:
+				print "exception saving file ", aFile
 
-		for aFile in os.listdir(img_dir):
-			#print aFile
-			if aFile.endswith(image_format):
-				#print "this is a jpg"
-				img_file = Image.open(img_dir+"/"+aFile).convert('RGB')
-				#print "img file: ", img_file
-				width, height = (int(scale_img * img_file.size[0] / 100.0), 
-														int(scale_img * img_file.size[1] / 100.0))
-
-				print "resizing: ", aFile, " to width ", width, " x height ", height 
-				img_file = img_file.resize((width, height), Image.ANTIALIAS)
-				try:
-					fileName = os.path.splitext(aFile)[0] #without file extension
-					img_file.save(img_dir+"/"+ fileName +"_resize.png",format="png", 
-												optimize=True, 
-												quality=100, 
-												progressive=True)
-					for i in os.listdir(img_dir):
-						if i.endswith('_resize.png'):
-							print i
-							src_file = os.path.join(img_dir, i)
-							dst_file = os.path.join(subdirectory, i)
-							shutil.move(src_file, dst_file)
-					#print img_files
-				
-				except IOError:
-					print "exception saving file ", aFile
-
-		'''     
-		for i in range(len(img_files)):
-				img_file = Image.open(img_dir+"/"+img_files[i])
-				print "img_file:", img_file
-				
-				if scale_img == 100:
-						width, height = img_file.size
-				else:
-						width, height = (int(scale_img * img_file.size[0] / 100.0), 
-														int(scale_img * img_file.size[1] / 100.0))
-
-				img_file = img_file.resize((width, height), Image.ANTIALIAS)
-				try:
-						img_file.save(img_dir+"/"+img_files[i], 
-													optimize=True, 
-													quality=img_quality, 
-													progressive=True)
-				except IOError:
-						ImageFile.MAXBLOCK = width * height
-						img_file.save(img_dir+"/"+img_files[i], 
-													optimize=True, 
-													quality=img_quality, 
-													progressive=True)
-			'''
 #img_dir = "C:/Users/Bears/Downloads/Ant_Videos"
 root = Tkinter.Tk()
 root.withdraw() # remove main app window in background
