e90555610b66ed53a11d7a0ebc65520a3b3eaa6e
blam4521 <blam452@gmail.com>
2015-10-23 23:13:47 -0700

    added copy files to dir 'texture_large'
    makes a copy of files into a folder called 'texture large', then goes
    through original files and turns image.tga to image.png, removes the
    .tga file.

--- Convert_longHand_b.py
+++ Convert_longHand_b.py
@@ -3,66 +3,75 @@ import os.path
 from PIL import Image, ImageFile
 import Tkinter, tkFileDialog
 import shutil
+import logging
 
 
-def convert_jpg(img_dir, image_format, scale_img):
+def convert_tga(img_dir, scale_img):
 
 	'''
-	Takes a img_dir, looks through every image file extension set by the users and changes it according to their
-	input scale
+	Takes a img_dir, copies the entire files in dir then looks through every orginal 
+	image file extension tga and changes it according user input input scale
 	'''
-	subdirectory = img_dir + "/texture_small"
-	
-	if os.path.exists(subdirectory):
-		print 'there is already a folder named %s'%(subdirectory)
-		
+	#copy the original images into a backup directory
+	large_subdir = img_dir + "/texture_large"
+	'''
+	if os.path.exists(large_subdir):
+		print 'there is already a folder named %s'%(large_subdir)
 	else:
 		print 'folder not created, creating folder'
-		makeDir = os.makedirs(subdirectory)
+		makeDir = os.makedirs(large_subdir)
+	'''
+	shutil.copytree(img_dir, large_subdir)
 
-	
+	'''
+	ext = ['png','jpg', 'vrimg', 'hdr','exr', 'tga', 'bmp', 'sgi','tif', 'vrst']
 
+			for i in os.listdir(img_dir):
+					if i.endswith(tuple(ext)):
+						print i
+						src_file = os.path.join(img_dir, i)
+						dst_file = os.path.join(large_subdir, i)
+						shutil.move(src_file, dst_file)
+	'''
 	for aFile in os.listdir(img_dir):
-		#print aFile
-		if aFile.endswith(image_format):
-			#print "this is a jpg"
+		
+		if aFile.endswith('tga'):
+			
 			img_file = Image.open(img_dir+"/"+aFile).convert('RGB')
-			#print "img file: ", img_file
+			
 			width, height = (int(scale_img * img_file.size[0] / 100.0), 
 													int(scale_img * img_file.size[1] / 100.0))
 
 			print "resizing: ", aFile, " to width ", width, " x height ", height 
 			img_file = img_file.resize((width, height), Image.ANTIALIAS)
+			
 			try:
+				#logging.basicConfig()
 				fileName = os.path.splitext(aFile)[0] #without file extension
-				img_file.save(subdirectory+"/"+ fileName +"_resize.png",format="png", 
+				
+				img_file.save(img_dir+"/"+ fileName +".png",format="png", 
 											optimize=True, 
 											quality=100, 
 											progressive=True)
-				for i in os.listdir(img_dir):
-					if i.endswith('_resize.png'):
-						print i
-						src_file = os.path.join(img_dir, i)
-						dst_file = os.path.join(subdirectory, i)
-						shutil.move(src_file, dst_file)
-				#print img_files
-			
+				os.remove(img_dir+"/"+aFile)
+				
 			except IOError:
 				print "exception saving file ", aFile
+
 
 #img_dir = "C:/Users/Bears/Downloads/Ant_Videos"
 root = Tkinter.Tk()
 root.withdraw() # remove main app window in background
 img_dir = tkFileDialog.askdirectory(parent=root,initialdir="C:/",title='Please select a directory')
 print "Selected directory: ", img_dir
-image_format = raw_input('Enter a file format: ')
-print image_format
+#image_format = raw_input('Enter a file format: ')
+#print image_format
 scale_img = int(input('Enter a scale percent (0-100): '))
 
-print "resizing %s..." %(image_format)
+print "resizing tga..." 
 if scale_img == 100:
 	print "scale of 100 has no effect, exiting program"
 else:
-	convert_jpg(img_dir, image_format, scale_img);
+	convert_tga(img_dir, scale_img);
 print "...done"
 
