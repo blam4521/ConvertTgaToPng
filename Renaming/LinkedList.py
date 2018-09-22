4efb2e201a5647807cb3242789f12af5434ad366
Barry Lam <blam452@gmail.com>
2018-09-22 13:06:50 -0400

    Not done with the link list class

--- Renaming/LinkedList.py
+++ Renaming/LinkedList.py
@@ -12,3 +12,30 @@ class LinkedList(object):
     def __init__(self):
         self.head = None
         self.size = 0
+
+    def insertStart(self, data):
+
+        self.size = self.size + 1
+        newNode = Node(data)
+
+        if not self.head:
+            self.head = newNode
+        else:
+            newNode.nextNode = self.head
+            self.head = newNode
+
+    # O(1)
+    def size(self):
+        return self.size
+
+    # O(n) not good !!
+    def size2(self):
+
+        actualNode = self.head
+        size = 0
+
+        while actualNode is not None:
+            size += 1
+            actualNode = actualNode.nextNode
+
+        return size
